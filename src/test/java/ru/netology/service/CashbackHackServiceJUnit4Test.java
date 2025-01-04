4c420510e2444c7f6db9cadc31b9766c17cdeda0
Olga <oli.samsonowa@yandex.ru>
2025-01-04 22:33:44 +0300

    Добавлены автотесты для JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -1,4 +1,40 @@
 package ru.netology.service;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceJUnit4Test {
+
+    @Test
+    public void shouldReturn900IfAmountIs100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0; // Ожидаем, что докупать ничего не нужно
+
+        assertEquals(expected, actual); // Этот тест упадёт из-за ошибки в методе remain()
+    }
+
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
 }
